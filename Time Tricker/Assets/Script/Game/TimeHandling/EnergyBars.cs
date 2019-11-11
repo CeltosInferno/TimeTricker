5d3d9f6badda177bed229e5851c9aad25057d914
Timothee Corsini <timothee.corsini@gmail.com>
2019-11-11 14:42:52 -0500

    power cost and feedback with energy bars

---
+++ Time Tricker/Assets/Script/Game/TimeHandling/EnergyBars.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyBars : MonoBehaviour
+{
+    public HealthBar slowBar;
+    public HealthBar speedBar;
+
+    public void setSize(float normalizedEnergy)
+    {
+        slowBar.SetSize(normalizedEnergy);
+        speedBar.SetSize(1f - normalizedEnergy);
+    }
+}
--- Time Tricker/Assets/Script/Game/TimeHandling/TimeController.cs
+++ Time Tricker/Assets/Script/Game/TimeHandling/TimeController.cs
@@ -9,30 +9,87 @@ public class TimeController : MonoBehaviour
     public float SlowValue = 0.5f;
     public float SpeedValue = 2f;
 
+    public float totalPower = 300f;
+    public float activatePowerCost = 15f;
+    public float costPerSeconds = 10f;
+
+    public EnergyBars energyBars;
+
+    float currentEnergy;
+
+    bool isSlowingDowm = false;
+    bool isSpeedingUp = false;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        currentEnergy = totalPower / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        InputGestion();
+        CalculateEnergy();
+        energyBars.setSize(currentEnergy / totalPower);
+    }
+
+    void CalculateEnergy()
+    {
+        float cost = costPerSeconds * Time.deltaTime;
+        if (isSlowingDowm) currentEnergy = Mathf.Max(0f, currentEnergy - cost);
+        else if (isSpeedingUp) currentEnergy = Mathf.Min(totalPower, currentEnergy + cost);
+        if (currentEnergy <= 0f || currentEnergy >= totalPower)
+        {
+            disactivatePower();
+        }
+    }
+    
+    void disactivatePower()
+    {
+        TimeManager.globalTimeMultiplier = 1f;
+        isSpeedingUp = false;
+        isSlowingDowm = false;
+    }
+
+    void activatePower(bool speedUp)
+    {
+        if(speedUp)
+        {
+            TimeManager.globalTimeMultiplier = SpeedValue;
+            isSpeedingUp = true;
+            isSlowingDowm = false;
+            currentEnergy += activatePowerCost;
+        }
+        else
+        {
+            TimeManager.globalTimeMultiplier = SlowValue;
+            isSpeedingUp = false;
+            isSlowingDowm = true;
+            currentEnergy -= activatePowerCost;
+        }
+    }
+
+    void InputGestion()
+    {
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            if (TimeManager.globalTimeMultiplier > 1f) TimeManager.globalTimeMultiplier = 1f;
-            else TimeManager.globalTimeMultiplier = SpeedValue;
+            if (isSpeedingUp) disactivatePower();
+            else if (currentEnergy < totalPower - activatePowerCost)
+                activatePower(true);
         }
         //speed up button
         else if (Input.GetKeyDown(KeyCode.E))
         {
-            if (TimeManager.globalTimeMultiplier < 1f) TimeManager.globalTimeMultiplier = 1f;
-            else TimeManager.globalTimeMultiplier = SlowValue;
+            if (isSlowingDowm) disactivatePower();
+            else if (currentEnergy > 0 + activatePowerCost)
+                activatePower(false);
         }
         //clear state
         else if (Input.GetKeyDown(KeyCode.W))
         {
-            TimeManager.globalTimeMultiplier = 1f;
+            disactivatePower();
         }
     }
 }
